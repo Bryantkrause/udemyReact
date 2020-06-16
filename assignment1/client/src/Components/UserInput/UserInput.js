fcf39d041bb149f56988a0725aef5ce9f35b3885
bryant <bryantkrause@gmail.com>
2020-06-16 10:07:31 -0700

    removed bloatware added userinput

---
+++ assignment1/client/src/Components/UserInput/UserInput.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './UserInput.css'
+
+const UserInput = (props) => {
+    return (
+        <div className='Input'>
+            <p>Enter Username</p>
+<input type="text" name="fname"></input>
+<button> Submit</button>
+       </div>
+    )
+}
+
+
+export default UserInput
